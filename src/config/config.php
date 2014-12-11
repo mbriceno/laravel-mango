86fa266117e4fb67abee5b8af94e5fc25b60a3aa
Mauro Casas <casas.mauroluciano@gmail.com>
2014-12-11 03:30:06 -0300

    Somtimes, I'm stupid.

--- src/config/config.php
+++ src/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-    'public' => 'public_test_uk7fji6cdi77zhkl7sehpok1tux1de92',
-    'secret' => 'secret_test_rh8rbxxrik73y0af2o2aq411ss8uk7pl',
+    'public' => '',
+    'secret' => '',
     'endpoint' => 'https://api.getmango.com/v1/',
 );
